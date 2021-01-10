bf42388d66c99fa3a6a93052d6a2b2331d07120b
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-01-10 14:24:39 -0500

    Implement conditional rendering of navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import { signout, isAuth } from "../components/helpers/auth";
 
 const isActive = (history, path) => {
   if (history.location.pathname === path)
-  return "text-primary border border-primary"
-  else return "text-dark"
+    return "text-primary border border-primary";
+  else return "text-dark";
   //  return { color: "#ff9900" };
   // else return { color: "#ffffff" };
 };
@@ -12,38 +13,53 @@ const isActive = (history, path) => {
 function Navbar({ history }) {
   return (
     <div
-      className={`navbar navbar-expand-lg  ${isActive(
-        history,
-        "/"
-      )}  navbar-light bg-white justify-content-between`}
+      className={`navbar navbar-expand-lg navbar-light bg-white justify-content-between`}
     >
       <Link class="navbar-brand" to="/">
         <img src="./logo.png" width="100" height="auto" alt="Gather logo" />
       </Link>
 
       <ul className="nav">
-        <li className="nav-item">
-          <Link
-            className={`nav-link bg-light ${isActive(
-              history,
-              "/signin"
-            )} rounded ml-1`}
-            to="/signin"
-          >
-            Sign In
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            className={`nav-link bg-light ${isActive(
-              history,
-              "/signup"
-            )} rounded ml-1`}
-            to="/signup"
-          >
-            Sign up
-          </Link>
-        </li>
+        {!isAuth() && (
+          <>
+            <li className="nav-item">
+              <Link
+                className={`nav-link bg-light ${isActive(
+                  history,
+                  "/signin"
+                )} rounded ml-1`}
+                to="/signin"
+              >
+                Sign In
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link bg-light ${isActive(
+                  history,
+                  "/signup"
+                )} rounded ml-1`}
+                to="/signup"
+              >
+                Sign up
+              </Link>
+            </li>
+          </>
+        )}
+        {isAuth() && (
+          <li className="nav-item">
+            <a
+              className={`nav-link bg-light ${isActive(
+                history,
+                "/signout"
+              )} rounded ml-1`}
+              onClick={() => signout(() => history.push("/"))}
+              style={{ cursor: "pointer" }}
+            >
+              Sign out
+            </a>
+          </li>
+        )}
       </ul>
     </div>
   );
--- src/components/helpers/auth.js
+++ src/components/helpers/auth.js
@@ -1,11 +1,42 @@
+import axios from "axios";
 
 // response is an object contianing token
 // and user info received when user successfully signs in
 // The callback function (next) is used for
 // redirecting user to another page after successfully setting cookie and local storage
-export const authenticate= (jwt, next) => {
-  if(typeof window !=="undefined") {
-    localStorage.setItem("jwt", JSON.stringify(jwt))
-    next()
+export const authenticate = (jwt, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(jwt));
+    next();
   }
-}
+};
+
+export const signout = async (next) => {
+  if (typeof window !== "undefined") {
+    // Remove token from local storage
+    localStorage.removeItem("jwt");
+    next();
+    try {
+      // Send request to backend to clear cookie
+      const response = await axios.get(`${process.env.REACT_APP_API}/signout`);
+      console.log("Sign out response from server ====>", response.data.message);
+      return response.data.message;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+// Function for authenticating user
+// Access user info from local storage
+export const isAuth = () => {
+  if(typeof window =="undefined") {
+    return false
+  }
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    // user is not authenticated
+    return false;
+  }
+};
