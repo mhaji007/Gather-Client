2f608fa1571ef2e6ea0fa8b08e9974017701b1d7
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-01-10 11:54:07 -0500

    Add style to active navabr link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,52 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
-function Navbar() {
+const isActive = (history, path) => {
+  if (history.location.pathname === path)
+  return "text-primary border border-primary"
+  else return "text-dark"
+  //  return { color: "#ff9900" };
+  // else return { color: "#ffffff" };
+};
+
+function Navbar({ history }) {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/signin">Sign In</Link>
-      <Link to="/signup">Sign up</Link>
+    <div
+      className={`navbar navbar-expand-lg  ${isActive(
+        history,
+        "/"
+      )}  navbar-light bg-white justify-content-between`}
+    >
+      <Link class="navbar-brand" to="/">
+        <img src="./logo.png" width="100" height="auto" alt="Gather logo" />
+      </Link>
+
+      <ul className="nav">
+        <li className="nav-item">
+          <Link
+            className={`nav-link bg-light ${isActive(
+              history,
+              "/signin"
+            )} rounded ml-1`}
+            to="/signin"
+          >
+            Sign In
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={`nav-link bg-light ${isActive(
+              history,
+              "/signup"
+            )} rounded ml-1`}
+            to="/signup"
+          >
+            Sign up
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
 
-export default Navbar;
+export default withRouter(Navbar);
