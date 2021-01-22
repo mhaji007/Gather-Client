79322e7475c4042518ad1961ae7132aeebf7e400
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-01-22 11:51:47 -0500

    Add loader to posts and single post pages

--- src/pages/post/Posts.js
+++ src/pages/post/Posts.js
@@ -8,7 +8,8 @@ import {
   CardSubtitle,
 } from "reactstrap";
 import { Link } from "react-router-dom";
-import {isAuth} from "../../components/helpers/auth";
+import loader from "../../loader.gif";
+import { isAuth } from "../../components/helpers/auth";
 import axios from "axios";
 
 function Posts() {
@@ -28,50 +29,63 @@ function Posts() {
   }, []);
 
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        {posts.map((post, i) => {
-          const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
-          const posterName = post.postedBy ? post.postedBy.name : "Unknown";
-          return (
-            <Card key={i} className="col-md-3 ml-2 mb-2">
-              <CardImg
-              className="img-thumbnail mb-3"
-              top
-              width="100%"
-              // height="320vw"
-              objectFit="cover"
-              src={`${process.env.REACT_APP_API}/post/photo/${
-                post._id
-              }?${new Date().getTime()}`}
-              alt={post.title}
-              onError={(i) => (i.target.src = "/avatar.png")}
-              style={{ height: "300px" }}
-            />
-              <CardBody>
-                <CardTitle tag="h5">{post.title}</CardTitle>
-                {/* <CardSubtitle tag="h6" className="mb-2 text-muted">
+    <>
+      {!posts.length ? (
+        <div className="text-center">
+          <img src={loader} style={{ width: "auto", height: "350px" }} />
+        </div>
+      ) : (
+        <div className="container">
+          <div className="row justify-content-center">
+            {posts.map((post, i) => {
+              const posterId = post.postedBy
+                ? `/user/${post.postedBy._id}`
+                : "";
+              const posterName = post.postedBy ? post.postedBy.name : "Unknown";
+              return (
+                <Card key={i} className="col-md-3 ml-2 mb-2">
+                  <CardImg
+                    className="img-thumbnail mb-3"
+                    top
+                    width="100%"
+                    // height="320vw"
+                    objectFit="cover"
+                    src={`${process.env.REACT_APP_API}/post/photo/${
+                      post._id
+                    }?${new Date().getTime()}`}
+                    alt={post.title}
+                    onError={(i) => (i.target.src = "/avatar.png")}
+                    style={{ height: "300px" }}
+                  />
+                  <CardBody>
+                    <CardTitle tag="h5">{post.title}</CardTitle>
+                    {/* <CardSubtitle tag="h6" className="mb-2 text-muted">
                 Card subtitle
               </CardSubtitle> */}
-                <CardText>{post.body.length >100 ? post.body.substring(0,100)+"...":post.body}</CardText>
-                <br />
-                <Link
-                  to={`/post/${post._id}`}
-                  className="btn btn-sm border border-dark rounded text-dark"
-                >
-                  Read more
-                </Link>
-                <p className="font-italic mt-3">
-                  Posted by {" "}
-                  <Link to={`${posterId}`}>{posterName}{" "}</Link>
-                  on {new Date(post.created).toDateString()}
-                </p>
-              </CardBody>
-            </Card>
-          );
-        })}
-      </div>
-    </div>
+                    <CardText>
+                      {post.body.length > 100
+                        ? post.body.substring(0, 100) + "..."
+                        : post.body}
+                    </CardText>
+                    <br />
+                    <Link
+                      to={`/post/${post._id}`}
+                      className="btn btn-sm border border-dark rounded text-dark"
+                    >
+                      Read more
+                    </Link>
+                    <p className="font-italic mt-3">
+                      Posted by <Link to={`${posterId}`}>{posterName} </Link>
+                      on {new Date(post.created).toDateString()}
+                    </p>
+                  </CardBody>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/pages/post/SinglePost.js
+++ src/pages/post/SinglePost.js
@@ -1,4 +1,4 @@
-import React , {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   CardImg,
@@ -7,84 +7,86 @@ import {
   CardTitle,
   CardSubtitle,
 } from "reactstrap";
+import loader from "../../loader.gif";
 import { Link } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
 
 function SinglePost({ match: { params } }) {
+  const [state, setState] = useState({
+    post: "",
+  });
 
-  const [state, setstate] = useState({
-    post:""
-  })
-
-  const {post} = state;
+  const { post } = state;
 
   useEffect(() => {
-    getPost()
-  }, [])
+    getPost();
+  }, []);
 
-  const getPost = async ( ) =>{
-       try {
-         const response = await axios.get(`${process.env.REACT_APP_API}/post/${params.postId}`);
-         console.log("Response from SinglePost =====>", response.data)
-         setstate({...state, post:response.data});
-       } catch (error) {
-         console.log("error from users", error);
-       }
-  }
+  const getPost = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API}/post/${params.postId}`
+      );
+      console.log("Response from SinglePost =====>", response.data);
+      setState({ ...state, post: response.data });
+    } catch (error) {
+      console.log("error from users", error);
+    }
+  };
 
   const renderPost = (post) => {
-          const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
-          const posterName = post.postedBy ? post.postedBy.name : "Unknown";
-   return (
-     <Card>
-       <CardImg
-         className="img-thumbnail mb-3"
-         top
-         width="100%"
-         // height="320vw"
-         objectFit="cover"
-         src={`${process.env.REACT_APP_API}/post/photo/${
-           post._id
-         }?${new Date().getTime()}`}
-         alt={post.title}
-         onError={(i) => (i.target.src = "/avatar.png")}
-         style={{ height: "300px", objectFit:"cover" }}
-       />
-       <CardBody>
-         {/* <CardTitle tag="h5">{post.title}</CardTitle> */}
-         {/* <CardSubtitle tag="h6" className="mb-2 text-muted">
+    const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
+    const posterName = post.postedBy ? post.postedBy.name : "Unknown";
+    return (
+      <Card>
+        <CardImg
+          className="img-thumbnail mb-3"
+          top
+          width="100%"
+          // height="320vw"
+          objectFit="cover"
+          src={`${process.env.REACT_APP_API}/post/photo/${
+            post._id
+          }?${new Date().getTime()}`}
+          alt={post.title}
+          onError={(i) => (i.target.src = "/avatar.png")}
+          style={{ height: "300px", objectFit: "cover" }}
+        />
+        <CardBody>
+          {/* <CardTitle tag="h5">{post.title}</CardTitle> */}
+          {/* <CardSubtitle tag="h6" className="mb-2 text-muted">
                 Card subtitle
               </CardSubtitle> */}
-         <CardText>
-           {post.body}
-         </CardText>
-         <br />
-         <Link
-           to={`/`}
-           className="btn btn-sm border border-dark rounded text-dark"
-         >
-           Back to Posts
-         </Link>
-         <p className="font-italic mt-3">
-           Posted by <Link to={`${posterId}`}>{posterName} </Link>
-           on {new Date(post.created).toDateString()}
-         </p>
-       </CardBody>
-     </Card>
-   );
-  }
+          <CardText>{post.body}</CardText>
+          <br />
+          <Link
+            to={`/`}
+            className="btn btn-sm border border-dark rounded text-dark"
+          >
+            Back to Posts
+          </Link>
+          <p className="font-italic mt-3">
+            Posted by <Link to={`${posterId}`}>{posterName} </Link>
+            on {new Date(post.created).toDateString()}
+          </p>
+        </CardBody>
+      </Card>
+    );
+  };
 
   return (
     <div className="container">
-    <h2 className="display-4 mt-5 mb-5">
-      {post.title}
-    </h2>
+      <h2 className="display-4 mt-5 mb-5">{post.title}</h2>
+      {!post ? (
+        <div className="text-center">
+          <img src={loader} style={{ width: "auto", height: "350px" }} />
 
-    {renderPost(post)}
-
+        </div>
+      ) : (
+        renderPost(post)
+      )}
     </div>
-  )
-
+  );
 }
 
 export default SinglePost;
