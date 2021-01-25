090b308ac2d9b76720aa3c8ea3b86de61659be62
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-01-25 13:27:14 -0500

    Implement update post functionality

---
+++ src/components/forms/EditPostForm.js
@@ -0,0 +1,154 @@
+import React from "react";
+import styles from "./EditPostForm.module.css";
+import { isAuth, updateUser } from "../../components/helpers/auth";
+import axios from "axios";
+
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+// import { GoogleLoginButton } from "react-social-login-buttons";
+
+const EditPostForm = ({ state, setState }) => {
+  const {
+    id,
+    title,
+    body,
+    success,
+    error,
+    photo,
+    loading,
+    postData,
+    fileSize,
+    buttonText,
+    redirectToProfile,
+  } = state;
+
+  console.log("form data from EditPostForm =======>", postData);
+  // Client side validation of user input
+  // for profile update.
+  const isValid = () => {
+    const { title, body, fileSize } = state;
+
+    // if (fileSize > 100000) {
+    //   setState({ ...state, error: "File size should be less than 100kb", loading:false });
+    //   return false;
+    // }
+    if (title.length === 0 || body.length === 0) {
+      setState({ ...state, error: "All fields are required", loading: false });
+      return false;
+    }
+
+    return true;
+  };
+
+  // Dynamic onChange handler
+  // used by all inputs
+  const handleChange = (name) => (e) => {
+    const value = name === "photo" ? e.target.files[0] : e.target.value;
+    const fileSize = name === "photo" ? e.target.files[0].size : 0;
+    const imageName =
+      name === "photo" ? e.target.files[0].name : "Upload Photo";
+
+    // Set dynamic name and value in formData
+    // have the formData ready to send to backend
+    // on submit
+
+    // if name:email => value of email
+    // if name:photo => value of photo
+    postData.set(name, value);
+
+    setState({
+      ...state,
+      [name]: value,
+      fileSize,
+      error: "",
+      success: "",
+      imageUploadText: imageName,
+    });
+  };
+
+  // Function reponsible for making request to post endpoint
+
+  const update = async () => {
+    const postId = id;
+    const token = isAuth().data.token;
+    try {
+      const response = await axios.put(
+        `${process.env.REACT_APP_API}/post/${postId}`,
+        // data to create a post
+        postData,
+
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      setState({
+        ...state,
+        title: "",
+        body: "",
+        photo: "",
+        buttonText: "Posted",
+        success: response.data.message,
+        loading: false,
+        imageUploadText: "Upload Image",
+        redirectToProfile: true,
+      });
+
+      console.log("POST created ===>", response.data);
+    } catch (error) {
+      console.log(error);
+      setState({
+        ...state,
+        buttonText: "Post",
+        error: error.response.data.error,
+        loading: false,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setState({ ...state, buttonText: "Posting...", loading: true });
+
+    if (isValid()) {
+      update();
+    }
+  };
+
+  return (
+    <>
+      <Form className={styles.edit} onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label> Photo </Label>
+          <Input
+            type="file"
+            accept="image/*"
+            onChange={handleChange("photo")}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label> Title </Label>
+          <Input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={handleChange("title")}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label> Body </Label>
+          <Input
+            type="textarea"
+            placeholder="Body"
+            value={body}
+            onChange={handleChange("body")}
+          />
+        </FormGroup>
+        <Button className="btn-lg btn-dark btn-block mt-3">Update Post</Button>
+      </Form>
+    </>
+  );
+};
+
+export default EditPostForm;
--- src/pages/post/EditPost.js
+++ src/pages/post/EditPost.js
@@ -1,12 +1,118 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+// import {
+//   Card,
+//   CardImg,
+//   CardText,
+//   CardBody,
+//   CardTitle,
+//   CardSubtitle,
+// } from "reactstrap";
+// import axios from "axios";
+// import { Link, Redirect } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import EditPostForm from "../../components/forms/EditPostForm";
+import {
+  showSuccessMessage,
+  showErrorMessage,
+} from "../../components/helpers/alerts";
+import loader from "../../loader.gif";
+
+import { isAuth } from "../../components/helpers/auth";
+import axios from "axios"
 
 function EditPost({match:{params}}) {
-  return (
-    <div>
-      <h2>Edit Post</h2>
-      {params.postId}
+
+const [state, setState] = useState({
+  id: "",
+  title: "",
+  body: "",
+  success: "",
+  error: "",
+  loading: false,
+  postData: new FormData(),
+  redirectToProfile: false,
+});
+
+
+  const {
+    title,
+    body,
+    photo,
+    success,
+    error,
+    user,
+    postData,
+    loading,
+    redirectToProfile,
+  } = state;
+
+
+useEffect(() => {
+
+getPost(params.postId)
+}, [params.postId])
+
+
+  const getPost = async (postId) => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API}/post/${postId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${isAuth().data.token}`,
+          },
+        }
+      );
+      setState({
+        ...state,
+        id: response.data._id,
+        title: response.data.title,
+        body: response.data.body,
+       error:""
+      });
+    } catch (error) {
+      console.log("error from profile", error);
+      // User trying to access this
+      // resource is not authenticated
+      setState({ error:error.response.data.error, redirectToProfile: true });
+    }
+  };
+
+
+  return redirectToProfile ? (
+    <Redirect to={`/user/${isAuth().data.user._id}`} />
+  ) : (
+    <div className="container">
+      <h2 className="mt-5 mb-5 text-center">Edit post</h2>
+      {success && showSuccessMessage(success)}
+      {error && showErrorMessage(error)}
+      <div className="text-center">
+        {loading && (
+          <img src={loader} style={{ width: "auto", height: "250px" }} />
+        )}
+      </div>
+      <div className="text-center ">
+        {/*To display the image we need to provide src to img tag
+        so there is no need fetch from backend.
+        If we do want to fetch photo from server
+        we have to modify how data is returned from server,
+        currently we are returning image type in headers.
+        if we use fetch, we have to return image data as json response
+        and then use that in react to display the image.
+         */}
+        {/* <img
+          src={photoUrl}
+
+          style={{ width: "auto", height: "150px" }}
+          // if src is not found that is error.
+          // so if there is error use default image
+          onError={(i) => (i.target.src = "/avatar.png")}
+          className="img-thumbnail "
+        /> */}
+      </div>
+      <EditPostForm state={state} setState={setState} />
     </div>
-  )
+  );
 }
 
 export default EditPost
