d48aeac408e53117ca44498e7a2e7761f0034bde
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-01-26 14:00:15 -0500

    Implement like and unlike functionalities

--- src/pages/post/SinglePost.js
+++ src/pages/post/SinglePost.js
@@ -17,21 +17,41 @@ function SinglePost({ match: { params } }) {
   const [state, setState] = useState({
     post: "",
     redirectToHome: false,
+    like: false,
+    likes: 0,
   });
 
-  const { post, redirectToHome } = state;
+  const { post, redirectToHome, like, likes} = state;
+
+  console.log("likes from state on load ===>", likes)
 
   useEffect(() => {
     getPost();
   }, []);
+
+  // Function to check whether usre has already liked the post
+  // or not. Without this check in place the like count
+  // is increased on refresh and user can like the already liked post
+  // and increase the likes from 1 to 2 and only after liking
+  // the post again the count reverts back to 0
+  const checkLike = (likes) => {
+    const userId = isAuth().data.user._id;
+    console.log("likes from checkLike ====>", likes)
+    let match = likes.toString().indexOf(userId) !==-1
+    return match;
+  }
 
   const getPost = async () => {
     try {
       const response = await axios.get(
         `${process.env.REACT_APP_API}/post/${params.postId}`
       );
-      console.log("Response from SinglePost =====>", response.data);
-      setState({ ...state, post: response.data });
+      console.log("Response from getPost =====>", response.data);
+      console.log("Response likes from getPost =====>", response.data.likes);
+      console.log("Response like's length from getPost =====>", response.data.likes.length);
+      setState((state) =>({ ...state, likes: response.data.likes.length, post: response.data }));
+      console.log("likes after setting state from getPost ===>", likes)
+      setState((state) => ({ ...state, like: checkLike(response.data.likes) }));
     } catch (error) {
       console.log("error from users", error);
     }
@@ -50,14 +70,54 @@ function SinglePost({ match: { params } }) {
     });
   };
 
-    const deleteConfirmed = () => {
-      let answer = window.confirm(
-        "Are you sure you want to delete this post?"
+  const deleteConfirmed = () => {
+    let answer = window.confirm("Are you sure you want to delete this post?");
+    if (answer) {
+      deletePost();
+    }
+  };
+
+  const likeHandler = async (postId, userId) => {
+    try {
+      const response = await axios.put(
+        `${process.env.REACT_APP_API}/post/like`,
+        { postId, userId },
+        {
+          headers: {
+            Authorization: `Bearer ${isAuth().data.token}`,
+          },
+        }
       );
-      if (answer) {
-        deletePost()
-      }
-    };
+      setState({ ...state, like: !like, likes: response.data.likes.length });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const unLikeHandler = async (postId, userId) => {
+    try {
+      const response = await axios.put(
+        `${process.env.REACT_APP_API}/post/unlike`,
+        { postId, userId },
+        {
+          headers: {
+            Authorization: `Bearer ${isAuth().data.token}`,
+          },
+        }
+      );
+      setState({ ...state, like: !like, likes: response.data.likes.length });
+      console.log ("likes length after setting state ===>", likes)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const likeToggle = () => {
+    let callApi = like ? unLikeHandler : likeHandler;
+    const userId = isAuth().data.user._id;
+    const postId = post._id;
+    callApi(postId, userId);
+  };
 
   const renderPost = (post) => {
     const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
@@ -83,6 +143,7 @@ function SinglePost({ match: { params } }) {
           {/* <CardSubtitle tag="h6" className="mb-2 text-muted">
                 Card subtitle
               </CardSubtitle> */}
+          <h3 onClick={likeToggle}>{likes} Like</h3>
           <CardText>{post.body}</CardText>
           <br />
           <p className="font-italic mt-3">
